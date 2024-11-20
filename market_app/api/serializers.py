77b8f772115adbe8bec840206d8cf9e47e1ec286
unknown <j.quabeck@gmx.net>
2024-11-20 21:54:04 +0100

    modelSerializer

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -11,48 +11,41 @@ def validate_no_X_Y(value):
         raise serializers.ValidationError(errors)
     return value
 
-class MarketSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    location = serializers.CharField(max_length=255, validators=[validate_no_X_Y])
-    description = serializers.CharField()
-    net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
-
-    def create(self, validated_data):
-        return Market.objects.create(**validated_data)
+class MarketSerializer(serializers.ModelSerializer):    
+    class Meta:
+        model = Market
+        exclude = ['net_worth']
     
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.location = validated_data.get('location', instance.location)
-        instance.description = validated_data.get('description', instance.description)
-        instance.net_worth = validated_data.get('net_worth', instance.net_worth)
-        instance.save()
-        return instance
-    
-class SellerDetailsSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    contact_info = serializers.CharField()
-    # markets = MarketSerializer(many=True, read_only=True)
-    markets = serializers.StringRelatedField(many = True)
+class SellerSerializer(serializers.ModelSerializer):   
+    markets = MarketSerializer(many = True, read_only = True)
+    market_ids = serializers.PrimaryKeyRelatedField(
+        queryset = Market.objects.all(),
+        many = True,
+        write_only = True,
+        source = 'markets'
+    )
 
-class SellerCreateSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=255)
-    contact_info = serializers.CharField()
-    markets = serializers.ListField(child = serializers.IntegerField(), write_only = True)
+    class Meta:
+        model = Seller
+        exclude = []
 
-    def validate_markets(self, value):
-        markets = Market.objects.filter(id__in = value)
-        if len(markets) != len(value):
-            raise serializers.ValidationError("One or more Marketids not found!")
-        return value
+# class SellerCreateSerializer(serializers.Serializer):
+#     name = serializers.CharField(max_length=255)
+#     contact_info = serializers.CharField()
+#     markets = serializers.ListField(child = serializers.IntegerField(), write_only = True)
+
+#     def validate_markets(self, value):
+#         markets = Market.objects.filter(id__in = value)
+#         if len(markets) != len(value):
+#             raise serializers.ValidationError("One or more Marketids not found!")
+#         return value
     
-    def create(self, validated_data):
-        market_ids = validated_data.pop('markets')
-        seller = Seller.objects.create(**validated_data)
-        markets = Market.objects.filter(id__in = market_ids)
-        seller.markets.set(markets)
-        return seller
+#     def create(self, validated_data):
+#         market_ids = validated_data.pop('markets')
+#         seller = Seller.objects.create(**validated_data)
+#         markets = Market.objects.filter(id__in = market_ids)
+#         seller.markets.set(markets)
+#         return seller
     
 class ProductsSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
@@ -66,8 +59,8 @@ class ProductCreateSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     description = serializers.CharField()
     price = serializers.DecimalField(max_digits=50, decimal_places=2)
-    market = serializers.PrimaryKeyRelatedField(many = True, read_only = True)
-    seller = serializers.PrimaryKeyRelatedField(many = True, read_only = True)
+    market = serializers.PrimaryKeyRelatedField(queryset = Market.objects.all())
+    seller = serializers.PrimaryKeyRelatedField(queryset = Seller.objects.all())
 
     def validate_products(self, value):
         product = Product.objects.filter(id__in = value)
@@ -78,18 +71,6 @@ class ProductCreateSerializer(serializers.Serializer):
     def create(self, validated_data):
         market_ids = validated_data.pop('market')
         seller_ids = validated_data.pop('seller')
-        product = Product.objects.create(**validated_data)
-        markets = Market.objects.filter(id__in = market_ids)
-        seller = Seller.objects.filter(id__in = seller_ids)
-        product.market.set(markets),
-        product.seller.set(seller)
+        product = Product.objects.create(**validated_data, market = market_ids, seller = seller_ids)
         return product
         
-    
-# {
-# "name" : "Produkt1",
-# "description" : "Das erste Produkt",
-# "price" : "10.00",
-# "market" : [1],
-# "seller" : [1,2]
-# }
--- market_app/api/views.py
+++ market_app/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import MarketSerializer, SellerDetailsSerializer, SellerCreateSerializer, ProductsSerializer, ProductCreateSerializer
+from .serializers import MarketSerializer, SellerSerializer, ProductsSerializer, ProductCreateSerializer
 from market_app.models import Market, Seller, Product
 
 @api_view(['GET', 'POST'])
@@ -9,11 +9,11 @@ def markets_view(request):
 
     if request.method == 'GET':
         markets = Market.objects.all()
-        serializer = MarketSerializer(markets, many=True)
+        serializer = SellerSerializer(markets, many=True)
         return Response(serializer.data)
     
     if request.method == 'POST':
-        serializer= MarketSerializer(data=request.data)
+        serializer= SellerSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -48,17 +48,41 @@ def market_single_view(request, pk):
 def sellers_view(request):
     if request.method == 'GET':
         sellers = Seller.objects.all()
-        serializer = SellerDetailsSerializer(sellers, many=True)
+        serializer = SellerSerializer(sellers, many=True)
         return Response(serializer.data)
     
     if request.method == 'POST':
-        serializer= SellerCreateSerializer(data=request.data)
+        serializer= SellerSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
         
+@api_view(['GET', 'PUT', 'Delete'])
+def sellers_single_view(request, pk):
+    if request.method == 'GET':
+        seller = Seller.objects.get(pk=pk)
+        serializer = SellerSerializer(seller)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        seller = Seller.objects.get(pk=pk)
+        serializer = SellerSerializer(seller, data=request.data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=200)
+        else:
+            return Response(serializer.errors, status=400)
+
+    if request.method == 'DELETE':
+        seller = Seller.objects.get(pk=pk)
+        seller.delete()
+        return Response({'message' : 'Seller deleted successfully!'}, status = 204)
+
+        # serializer = SellerSerializer(seller)
+        # seller.delete()
+        # return Response(serializer.data)
 
 @api_view(['GET', 'POST'])
 def products_view(request):
@@ -75,5 +99,3 @@ def products_view(request):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
-
-        
